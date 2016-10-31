7c090a3566ce7040718cc0e755f95bf39c0a3fa8
blake6993 <blakeh47@hotmail.com>
2016-10-31 16:42:02 +0000

    move between scenes code

---
+++ Code/transition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Application.LoadLevel("instructions");
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
